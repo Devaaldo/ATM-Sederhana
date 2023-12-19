67375311514af2e7ac30f6b7c72a2444f1d1bfd0
Devaaldo <akbarprdn2512@gmail.com>
2023-12-19 20:56:28 +0700

    revisi sdikit

--- atm4.py
+++ atm4.py
@@ -14,40 +14,41 @@ class ATM:
             self.saldo -= jumlah_tarik
             return f"Penarikan berhasil. Sisa saldo: {self.saldo}"
         else:
-            return "Penarikan gagal. Anda terlalu miskin."
+            return "Penarikan gagal. Saldo anda tidak mencukupi."
     
     def cek_saldo(self):
         return self.saldo
 
-def main():
-    saldo_awal = 0
-    atm = ATM(saldo_awal)
-
-    while True:
-        print("======================")
-        print("     ATM REGAJAYA    ")
-        print("======================")
-        print("1. Simpan Uang")
-        print("2. Tarik Uang")
-        print("3. Cek Saldo")
-        print("4. Cancel")
-
-
-        pilihan = input("Masukkan nomor opsi yang diinginkan : ")
-
-        if pilihan == "1":
-            jumlah_setor = int(input("Masukkan jumlah uang yang ingin anda simpan :  "))
-            print(atm.simpan_uang(jumlah_setor))
-        elif pilihan == "2":
-            jumlah_tarik = int(input("Masukkan jumlah penarikan: "))
-            print(atm.tarik_uang(jumlah_tarik))
-        elif pilihan == "3":
-            print(f"Duit anda sekarang adalah : {atm.cek_saldo()}")
-        elif pilihan == "4":
-            print("Keluar dari ATM...")
-            break
-        else:
-            print("Pilihan anda tidak valid. Silahkan pilih opsi 1, 2 , 3, atau 4.")
-
 if __name__ == "__main__":
+    def main():
+        saldo_awal = 0
+        atm = ATM(saldo_awal)
+
+        while True:
+            print("======================")
+            print("     ATM REGAJAYA    ")
+            print("======================")
+            print("1. Simpan Uang")
+            print("2. Tarik Uang")
+            print("3. Cek Saldo")
+            print("4. Cancel")
+
+
+            pilihan = input("Masukkan nomor opsi yang diinginkan : ")
+
+            if pilihan == "1":
+                jumlah_setor = int(input("Masukkan jumlah uang yang ingin anda simpan :  "))
+                print(atm.simpan_uang(jumlah_setor))
+            elif pilihan == "2":
+                jumlah_tarik = int(input("Masukkan jumlah penarikan: "))
+                print(atm.tarik_uang(jumlah_tarik))
+            elif pilihan == "3":
+                print(f"Duit anda sekarang adalah : {atm.cek_saldo()}")
+            elif pilihan == "4":
+                print("Keluar dari ATM...")
+                break
+            else:
+                print("Pilihan anda tidak valid. Silahkan pilih opsi 1, 2 , 3, atau 4.")
+
+
     main()
